138221c59e41a35b0dd10121069f3279903065b2
root <root@localhost.localdomain>
2020-08-24 00:06:22 +0530

    Installation fix and player redirection

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 pip3 install -r requirements.txt
-echo firefox http://0.0.0.0:8000 > launch.sh 
+python3 manage.py migrate
+echo firefox http://127.0.0.1:8000 > launch.sh 
 echo python3 manage.py runserver >> launch.sh 
 chmod +x ./launch.sh && rm install.sh
