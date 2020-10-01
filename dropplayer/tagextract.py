b06f0e2aade1138dfdedb980eca4d50b82bef00d
gamegods3 <sudevssuresh@gmail.com>
2020-10-01 12:18:00 +0530

    fix rendering of default album art

--- dropplayer/tagextract.py
+++ dropplayer/tagextract.py
@@ -6,12 +6,12 @@ metadata = {
     "artist":"Unnamed Artist",
     "album":"Untitled Album"
 }
+with open('./static/dropplayer/images/albumartdefault.jpg', 'rb') as f:
+    trackbin = f.read()
 def TagExtract(song):
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    if song == None:
-        with open('./static/dropplayer/images/albumartdefault.jpg', 'rb') as f:
-            trackbin = f.read()
-            track_img = str(b64encode(trackbin))[2:-3]
+    if song == None: 
+        track_img = str(b64encode(trackbin))[2:-3]
     else:
         track = TinyTag.get(song, image=True)
         track_img = track.get_image()
@@ -24,6 +24,8 @@ def TagExtract(song):
             metadata["artist"] = artist
         if album != None:
             metadata["album"] = album
-       
-        track_img = str(b64encode(track_img))[2:-3]
+        if track_img != None:
+            track_img = str(b64encode(track_img))[2:-3]
+        else:
+            track_img = str(b64encode(trackbin))[2:-3]
     return metadata, track_img
