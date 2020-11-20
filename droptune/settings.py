6365cbe46f59bec24752677725abbdda1ef79754
gamegods3 <sudevssuresh@gmail.com>
2020-11-20 21:02:52 +0530

    Complete single device bullshit

--- droptune/settings.py
+++ droptune/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!$ogx1dxnm$@olyvpda0pfpt-pe260@lf%$57g#0e(lmzd5j!k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '172.20.10.2']
 
 
 # Application definition
